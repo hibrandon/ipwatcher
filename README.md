dcc1eb0cc2851ba36621795a6bb0550fd031b9c1
Mike Deats <mikedeats@gmail.com>
2012-07-30 00:00:45 -0600

    Added license data, and moved properties.conf to an example file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ipwatcher
 =========
+A simple app for watching a machine's external IP and notifying you via email or text when
+it changes.
+
+properties.conf
+===============
+The app is controlled by the properties file in the conf directory. Copy the included 
+properties.conf.example to properties.conf and modify the appropriate settings.
--- src/watchIP.py
+++ src/watchIP.py
@@ -1,4 +1,21 @@
+"""
+   Copyright 2012 Brandon Sutherlin, Mike Deats, and Ryan Neal
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+""" 
+
 import os
+
 from ConfigParser import SafeConfigParser
 
 from emailWrapper import EmailWrapper
@@ -19,10 +36,10 @@ class WatchIP:
         self.hostHasChanged = False
         self.exteralIpHasChanged = False
         self.internalIpHasChanged = False
-        
+
+
         self.parser = SafeConfigParser()
         self.parser.read(self.properties)
-        
         
         self.getPreviousIP()
         self.getCurrentIP()
